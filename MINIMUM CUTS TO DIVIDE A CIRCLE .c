63370665d816759b9cb76639ccdb1e958fd5102d
naveennaveen456321-dev <naveennaveen456321@gmail.com>
2026-02-06 15:11:47 +0530

    Update MINIMUM CUTS TO DIVIDE A CIRCLE .c

--- MINIMUM CUTS TO DIVIDE A CIRCLE .c
+++ MINIMUM CUTS TO DIVIDE A CIRCLE .c
@@ -1,1 +1,15 @@
+ int numberOfCuts(int n) {
+    if(n == 1){
+        return 0;
+    }
+    if(n%2==0)
+    {
+        n=n/2;
+        return n;
+    }
+    else
+    {
+        return n;
+    }
 
+}
