67b8a27aba835d1f9a276190ac1220d77057bbd7
naveennaveen456321-dev <naveennaveen456321@gmail.com>
2026-02-06 15:40:44 +0530

    Create Water Bottles.c

---
+++ Water Bottles.c
@@ -0,0 +1,14 @@
+int numWaterBottles(int numBottles, int numExchange)
+ {
+    {
+        int total=numBottles;
+        int empty=numBottles;
+    while(empty>=numExchange)
+    {
+        int newBottle=empty/numExchange;
+        total=total+newBottle;
+        empty=newBottle+(empty%numExchange);
+    }
+    return total;
+    }
+ }  
